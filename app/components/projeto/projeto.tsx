8ecff17a426adc52c05436df54d9c717de951231
Guilherme <81939515+Delarry021@users.noreply.github.com>
2023-05-07 20:59:36 -0300

    Update projeto.tsx

--- app/components/projeto/projeto.tsx
+++ app/components/projeto/projeto.tsx
@@ -9,7 +9,7 @@ interface ProjetoProps {
 
 export default function Projeto(props: ProjetoProps) {
     return(
-        <div className='flex border-b pb-52 sm:pb-24 border-b-zinc-300 dark:border-b-zinc-800 justify-around sm:px-16 w-full'>
+        <div className='flex border-b pb-52 sm:pb-24 border-b-zinc-300 dark:border-b-zinc-800 justify-between sm:px-16 w-full'>
             <div>
             <Image
             className='z-10'
@@ -18,7 +18,7 @@ export default function Projeto(props: ProjetoProps) {
             width={480}
             height={270}/>
             </div>
-            <div className='flex flex-col justify-around lg:pl-12 lg:pr-72  sm:px-8 rounded-lg'>
+            <div className='flex flex-col justify-around lg:pl-12 lg:pr-72  sm:px-2 rounded-lg'>
                 <div className='text-left flex flex-col sm:gap-0 gap-4'>
                     <h3 className='text-zinc-700 dark:text-zinc-50'>{props.Titulo}</h3>
                     <div className='flex items-center gap-8'>
